63f5ebd7067507522659c40cfdccc26a18a0e3f6
Marco Rivera Serrano <marco99eu@gmail.com>
2024-03-20 12:24:41 -0600

    Intento de leer el buffer y arreglos para manejar floats

--- Proyecto 1/ARM/write.s
+++ Proyecto 1/ARM/write.s
@@ -1,7 +1,16 @@
+.arch armv7-a
+.fpu vfpv3
+.eabi_attribute 67, "2.09"	@ EABI version
+.eabi_attribute 6, 10		@ Hard float ABI
+
 .section .data
 
 file_path:      .asciz "prueba.txt"
-buffer:         .space 1024      @ Tamaño del buffer para contener el contenido del archivo
+buffer:         .space 2205      @ Tamaño del buffer para contener el contenido del archivo
+ejemploFloat:	.float 4.566
+constante:	.float 75.0
+buffer_end:	.word 0
+.align 1
 
 .section .text
 
@@ -10,9 +19,10 @@ buffer:         .space 1024      @ Tamaño del buffer para contener el contenido
 _start:
     @ Abre el archivo
     ldr r0, =file_path
-    mov r1, #0      @ O_RDONLY (Modo de lectura)
+    mov r1, #2      @ O_RDONLY (Modo de lectura)
+    mov r2, #0
     mov r7, #5      @ Código de llamada al sistema para abrir el archivo
-    svc 0
+    swi 0
 
     @ Verifica si hubo un error al abrir el archivo (r0 contiene el descriptor de archivo o el código de error)
     cmp r0, #-1
@@ -31,9 +41,9 @@ loop:
     @ Lee un bloque de datos del archivo
     mov r0, r9
     ldr r1, =buffer
-    ldr r2, =1024    @ Lee hasta 1024 bytes a la vez
+    ldr r2, =2205    @ Lee hasta 1024 bytes a la vez
     mov r7, #3       @ Código de llamada al sistema para leer desde el archivo
-    svc 0
+    swi 0
 
     @ Verifica si se llegó al final del archivo (EOF)
     cmp r0, #0
@@ -45,21 +55,42 @@ loop:
     b loop
 
 preCalculo:
+
+    mov r3, r2
+    ldr r4, =2205
+    b print_loop
+
+print_loop:
+    ldrb r5, [r3] 
+
+    mov r0, #1
+    mov r1, r5
+    mov r2, #1
+    mov r7, #4
+    swi 0
+
+    add r3, r3, #1
+    subs r4, r4, #1
+    bne print_loop
+    b exit
+
+
     @ Imprime el contenido del buffer en la consola
-    mov r0, #1        @ Descriptor de archivo stdout
-    mov r1, r8
-    mov r2, r10
-    mov r7, #4        @ Código de llamada al sistema para escribir en la consola
-    svc 0
+    @mov r0, #1        @ Descriptor de archivo stdout
+    @mov r1, r8
+    @mov r2, r10
+    @mov r7, #4        @ Código de llamada al sistema para escribir en la consola
+    @swi 0
+
+exit:
 
     @ Cierra el archivo
     mov r0, r9
     mov r7, #6        @ Código de llamada al sistema para cerrar el archivo
-    svc 0
+    swi 0
 
-exit:
     mov r7, #1        @ Código de llamada al sistema para salir del programa
-    svc 0
+    swi 0
 
 error:
     @ Manejo de error al abrir el archivo

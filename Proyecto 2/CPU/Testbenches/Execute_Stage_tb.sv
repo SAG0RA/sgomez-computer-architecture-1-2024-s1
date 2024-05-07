b1f71dc9ea6b73c550d1023d5c57c53d436fa4e5
Joseph Jimenez <josephdjz@hotmail.com>
2024-05-06 22:00:31 -0600

    Execute Stages: Casos de Uso R,J,I
    Insertados 1 caso de uso para cada tipo de instrucción.

--- Proyecto 2/CPU/Testbenches/Execute_Stage_tb.sv
+++ Proyecto 2/CPU/Testbenches/Execute_Stage_tb.sv
@@ -95,86 +95,84 @@ module Execute_Stage_tb;
 	always #10 clk = ~clk;
 	
 	initial begin
+		$display("--------------------------------INSTRUCCION add rd=0, rs1=1, rs2=2 Tipo R--------------------------------\n");
 		// Ciclo 1:
       $display("Inicio del primer ciclo");
-      // Asigna valores simulados para las entradas del DecodeExecute_register
       ALUop_decode = 3'b001;
 		wbs_decode = 1; 
-		wme_decode = 1;
-		mm_decode = 1;
-      wm_decode = 1;
+		wme_decode = 0;
+		mm_decode = 0;
+      wm_decode = 0;
       am_decode = 0;
-      ni_decode = 1;
-      srcA_decode = 16'b0000000000000010;
-      srcB_decode = 16'b0000000000000011;
+      ni_decode = 0;
+      srcA_decode = 16'b0000000000000001;
+      srcB_decode = 16'b0000000000000000;
+		flagN = 0;
+		flagZ = 0;
       // Espera al próximo flanco de bajada del reloj
-      #10;  
-      $display("Resultados del primer ciclo:");
+      #20;  
 		$display("Entradas DecodeExecute_register:");
       $display("wbs_decode = %b, wme_decode = %b, mm_decode = %b", wbs_decode, wme_decode, mm_decode);
       $display("ALUop_decode = %b, wm_decode = %b, am_decode = %b, ni_decode = %b", ALUop_decode, wm_decode, am_decode, ni_decode);
       $display("srcA_decode = %h, srcB_decode = %h", srcA_decode, srcB_decode);
-		// Mostrar entradas y salidas del ExecuteMemory_register
       $display("Resultados del ExecuteMemory_register:");
+		$display("----------------------------------------------------------------\n");
       $display("wbs_memory = %b, wme_memory = %b, mm_memory = %b", wbs_memory, wme_memory, mm_memory);
       $display("alu_result_memory = %h, write_Data_memory = %h", alu_result_memory, write_Data_memory);
       $display("wm_memory = %b, ni_memory = %b", wm_memory, ni_memory);
-        
-      // Ciclo 2:
-      $display("\n \n \n");
+		$display("\n \n \n");
+      $display("--------------------------------INSTRUCCION beq Address = 0x8 Tipo J--------------------------------\n");
+		// Ciclo 2:
       $display("Inicio del segundo ciclo");
-      // Asigna nuevos valores simulados para las entradas del DecodeExecute_register
-      ALUop_decode = 3'b010;
+      ALUop_decode = 3'bxxx;
 		wbs_decode = 0; 
 		wme_decode = 0;
 		mm_decode = 0;
       wm_decode = 0;
-      am_decode = 1;
+      am_decode = 0;
       ni_decode = 0;
-      srcA_decode = 16'b1111000011110000;
-      srcB_decode = 16'b0000111100001111;
-		// Espera al próximo flanco de bajada del reloj
-      #20;
-		// Mostrar resultados
-      $display("Resultados del segundo ciclo:");
-      $display("Entradas DecodeExecute_register:");
+      srcA_decode = 16'b0000000000000000;
+      srcB_decode = 16'b0000000000001000;
+		flagN = 0;
+		flagZ = 0;
+      // Espera al próximo flanco de bajada del reloj
+      #20;  
+		$display("Entradas DecodeExecute_register:");
       $display("wbs_decode = %b, wme_decode = %b, mm_decode = %b", wbs_decode, wme_decode, mm_decode);
       $display("ALUop_decode = %b, wm_decode = %b, am_decode = %b, ni_decode = %b", ALUop_decode, wm_decode, am_decode, ni_decode);
       $display("srcA_decode = %h, srcB_decode = %h", srcA_decode, srcB_decode);
-		// Mostrar entradas y salidas del ExecuteMemory_register
       $display("Resultados del ExecuteMemory_register:");
+		$display("----------------------------------------------------------------\n");
       $display("wbs_memory = %b, wme_memory = %b, mm_memory = %b", wbs_memory, wme_memory, mm_memory);
       $display("alu_result_memory = %h, write_Data_memory = %h", alu_result_memory, write_Data_memory);
       $display("wm_memory = %b, ni_memory = %b", wm_memory, ni_memory);
-        
-      // Ciclo 3:
-      $display("\n \n");
+		$display("\n \n \n");
+		$display("--------------------------------INSTRUCCION str rd=14, Imma=3506 Tipo I--------------------------------\n");
+		// Ciclo 3:
       $display("Inicio del tercer ciclo");
-      // Asigna nuevos valores simulados para las entradas del DecodeExecute_register
-      ALUop_decode = 3'b011;
+      ALUop_decode = 3'bxxx;
 		wbs_decode = 1; 
 		wme_decode = 1;
 		mm_decode = 1;
       wm_decode = 1;
       am_decode = 1;
-      ni_decode = 1;
-      srcA_decode = 16'b0101010101010101;
-      srcB_decode = 16'b1010101010101010;
-      
+      ni_decode = 0;
+      srcA_decode = 16'b0000000000001110;
+      srcB_decode = 16'b0000110110110010;
+		flagN = 0;
+		flagZ = 1;
       // Espera al próximo flanco de bajada del reloj
-      #20;
-       
-      // Mostrar resultados
-      $display("Resultados del tercer ciclo:");
-      $display("Entradas DecodeExecute_register:");
+      #20;  
+		$display("Entradas DecodeExecute_register:");
       $display("wbs_decode = %b, wme_decode = %b, mm_decode = %b", wbs_decode, wme_decode, mm_decode);
       $display("ALUop_decode = %b, wm_decode = %b, am_decode = %b, ni_decode = %b", ALUop_decode, wm_decode, am_decode, ni_decode);
       $display("srcA_decode = %h, srcB_decode = %h", srcA_decode, srcB_decode);
-      // Mostrar entradas y salidas del ExecuteMemory_register
       $display("Resultados del ExecuteMemory_register:");
+		$display("----------------------------------------------------------------\n");
       $display("wbs_memory = %b, wme_memory = %b, mm_memory = %b", wbs_memory, wme_memory, mm_memory);
       $display("alu_result_memory = %h, write_Data_memory = %h", alu_result_memory, write_Data_memory);
       $display("wm_memory = %b, ni_memory = %b", wm_memory, ni_memory);
+		$display("\n \n \n");
       $finish;
     end
 
